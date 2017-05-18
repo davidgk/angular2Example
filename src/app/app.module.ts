b13cc7f0a0943f00b7321f14504b7302b441553a
davidgk <david.kotlirevsky@gmail.com>
2017-05-18 16:37:36 -0300

    nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MiNuevoComponenteComponent } from './mi-nuevo-componente/mi-nuevo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiNuevoComponenteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mi-nuevo-componente/mi-nuevo-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mi-nuevo-componente',
+  templateUrl: './mi-nuevo-componente.component.html',
+  styleUrls: ['./mi-nuevo-componente.component.css']
+})
+export class MiNuevoComponenteComponent implements OnInit {
+  valor="Que vuelva el carlos";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
